c36d7d455ba1493b9f38a841d4ed041f2fc356e8
baraju-design <137268112+baraju-design@users.noreply.github.com>
2025-05-19 22:42:57 +0300

    doing more tests

--- debug.py
+++ debug.py
@@ -1,17 +1,36 @@
 from lib.customer import Customer
 from lib.coffee import Coffee
 
-c1 = Customer("Bakari")
-c2 = Customer("Isaac")
+customer1 = Customer("Bakari")
+customer2 = Customer("Isaac")
+customer3 =Customer('John')
+customer4 = Customer('kizito')
 
 coffee1 = Coffee("Latte")
 coffee2 = Coffee("Espresso")
+coffee3=Coffee('Drip coffee')
 
-c1.create_order(coffee1, 4.5)
-c1.create_order(coffee2, 5.0)
-c2.create_order(coffee1, 4.0)
+customer1.create_order(coffee1, 4.5)
+customer1.create_order(coffee2, 5.0)
+customer2.create_order(coffee1, 4.0)
+customer4.create_order(coffee3,6.0)
+customer3.create_order(coffee3,9.0)
 
-print("Coffees ordered by Bakari:", [c.name for c in c1.coffees()])
+print("----------------------------------------------------------------")
+
+print("Coffees ordered by Bakari:", [c.name for c in customer1.coffees()])
 print("Customers who ordered Latte:", [c.name for c in coffee1.customers()])
 print("Latte total orders:", coffee1.num_orders())
 print("Latte average price:", coffee1.average_price())
+
+print("-----------------------------------------------------------------")
+
+print('\n ---Orders by customer---')
+for order in customer4.orders():
+    print(f"Customer:{order.customer.name},coffee:{order.coffee.name},price:${order.price:.2f}")
+for order in customer2.orders():
+    print(f"Customer:{order.customer.name},coffee:{order.coffee.name},price:${order.price:.2f}")    
+for order in customer1.orders():
+    print(f"Customer:{order.customer.name},coffee:{order.coffee.name},price:${order.price:.2f}")      
+for order in customer3.orders():
+    print(f"Customer:{order.customer.name},coffee:{order.coffee.name},price:${order.price:.2f}")      
